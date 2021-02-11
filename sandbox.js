f7838aa7d0f97f5a88e40a89e80d20c25eab10fd
Васильев Виталий <vasilev.v.vitalii@gmail.com>
2021-02-11 10:19:58 +0300

    ...

--- sandbox.js
+++
@@ -1,5 +0,0 @@
-//@ts-check
-
-let lib = require('./index.js')
-
-console.log(lib.depot_sch_table('dbo', 'aaa', undefined, [{name: 'a', type: 'nvarchar', len_chars: 150, pk_position: 1, collate: 'SQL_Latin1_General_CP1_CS_AS'}], 'ignore'))
